9bcc722b61500973d83583edf9a11286f04aa9b2
flowx08 <meronicarlo98@gmail.com>
2023-01-04 20:08:43 +0100

    Code update

---
+++ src/models/predict_model.py
@@ -0,0 +1,52 @@
+import argparse
+import sys
+import torch
+import torch.nn as nn
+import click
+
+from data import mnist
+from model import FCModel
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument("model_checkpoint")
+@click.argument("datafile")
+def predict(model_checkpoint):
+    print("Evaluating until hitting the ceiling")
+    print(model_checkpoint)
+
+    # TODO: Implement evaluation logic here
+    model = FCModel()
+    model.load_state_dict(torch.load(model_checkpoint))
+    _, test_set = mnist()
+
+    criterion = nn.NLLLoss()
+    model.eval()
+
+    transform = torchvision.transforms.Normalize((0.5,), (0.5,)),
+    x = torch.as_tensor(np.load(datafile))
+    x = transform(x)
+
+    logits = model(x)
+    loss = criterion(logits, y)
+    result = logits.argmax(dim=1)
+
+    print("Result: {}".format(result)
+
+
+cli.add_command(predict)
+
+
+if __name__ == "__main__":
+    cli()
+
+
+    
+    
+    
+    
