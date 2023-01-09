b3f2d4a69fb0a49369304e79150da48042b1127a
flowx08 <meronicarlo98@gmail.com>
2023-01-09 20:41:22 +0100

    Added unit test model

---
+++ tests/test_model.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('./src/data/')
+sys.path.append('./src/models/')
+
+from model import FCModel
+from hyperparameters import hyperparameters
+import math
+import torch
+
+def test_model():
+    x = torch.rand(1, 28, 28)
+    model = FCModel()
+    y = model.foreward(x)
+    assert(y.shape == torch.Size([1, 10]))
+
