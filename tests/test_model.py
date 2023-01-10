99381a2ffac6ddfda9a05df781045623f69e31fb
Mads <madsfpdk@gmail.com>
2023-01-10 10:38:34 +0100

    Black stuff added

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,15 +1,16 @@
 import sys
-sys.path.append('./src/data/')
-sys.path.append('./src/models/')
+
+sys.path.append("./src/data/")
+sys.path.append("./src/models/")
 
 from model import FCModel
 from hyperparameters import hyperparameters
 import math
 import torch
 
+
 def test_model():
     x = torch.rand(1, 28, 28)
     model = FCModel()
     y = model.foreward(x)
-    assert(y.shape == torch.Size([1, 10]))
-
+    assert y.shape == torch.Size([1, 10])
