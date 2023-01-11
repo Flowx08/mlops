420eb4b2feac8ba348573c85da3edd4ea1f8c082
flowx08 <meronicarlo98@gmail.com>
2023-01-11 18:19:52 +0100

    Fixed test typo + moved scripts in scripts folder

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,9 +10,18 @@ import torch
 
 
 def test_corrupted_mnist():
-    train, test = garbage_dataset(hyperparameters.batch_size, image_resize=hyperparameters.image_size)
-    assert(len(train) == 64)
-    assert(len(test) == 16)
+    train, test = garbage_dataset(
+        hyperparameters.batch_size, image_resize=hyperparameters.image_size
+    )
+    assert len(train) == 64
+    assert len(test) == 16
     x, y = next(iter(train))
-    assert(x.shape == torch.Size([hyperparameters.batch_size, 3, hyperparameters.image_size, hyperparameters.image_size]))
-    assert(y.shape == torch.Size([hyperparameters.batch_size]))
+    assert x.shape == torch.Size(
+        [
+            hyperparameters.batch_size,
+            3,
+            hyperparameters.image_size,
+            hyperparameters.image_size,
+        ]
+    )
+    assert y.shape == torch.Size([hyperparameters.batch_size])
--- tests/test_model.py
+++ tests/test_model.py
@@ -11,6 +11,8 @@ import torch
 
 def test_model():
     x = torch.rand(1, 3, hyperparameters.image_size, hyperparameters.image_size)
-    model = EfficientNetModel(hyperparameters.num_classes, hyperparameters.efficientnet_num)
-    y = model.foreward(x)
-    assert(y.shape == torch.Size([1, hyperparameters.num_classes]))
+    model = EfficientNetModel(
+        hyperparameters.num_classes, hyperparameters.efficientnet_num
+    )
+    y = model(x)
+    assert y.shape == torch.Size([1, hyperparameters.num_classes])
