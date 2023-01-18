3ee63605cbbeb458babdec7ca4784f5d9bcfd1d9
flowx08 <meronicarlo98@gmail.com>
2023-01-18 09:52:40 +0100

    Added hydra config file

--- src/models/hyperparameters.py
+++ src/models/hyperparameters.py
@@ -1,17 +1,2 @@
-class hyperparameters:
-    # Managing params
-    save_model_path = "./models/model.pth"
-    deploy_model_path = "./models/model_82_8.pth"
-
-    # Model hyperparameters
-    model_output_size = 10
-    model_filter_k = 10
-    image_size = 224
-    efficientnet_num = 0
-    num_classes = 6
-
-    # Training params
-    learningrate = 1e-4
-    epochs = 2
-    batch_size = 32
-    num_workers = 8
+from omegaconf import OmegaConf
+hyperparameters = OmegaConf.load('./src/models/config.yaml')
